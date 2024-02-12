a8d625ce2679332f66a4180740640af4697dae7c
Brandon Martin Del Campo <brandondevprog@gmail.com>
2024-02-11 20:14:17 -0800

    Flickering Added

--- Scripts/LightSystem/BaseLight.gd
+++ Scripts/LightSystem/BaseLight.gd
@@ -1,14 +1,19 @@
 extends SpotLight3D
 class_name BaseLight
 
+
+@export_enum("1","2","3","4","5","6","7","8","9","10","11","12","13","14") var SwitchToConnectTo: String
 @export var MinPowerLight = 1
 @export var MaxPowerOfLight = 2
 @export var On = true
 @export var FlickeringLoop :float
+@export var Flickering = false
+
+
+@onready var FlickerTimer=$Timer
+
 var SwitchConnect 
 var NoStopingFlicking = true
-@onready var FlickerTimer=$Timer
-@export_enum("1","2","3","4","5","6","7","8","9","10","11","12","13","14") var SwitchToConnectTo: String
 var rng = RandomNumberGenerator.new()
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -41,18 +46,20 @@ func ConnectToSwitch():
 
 func StartFlickingLights():
 	$Timer.Start()
-	
+	Flickering = true
 	
 	
 
 func _on_timer_timeout():
-	var my_random_number
-	if NoStopingFlicking:
-		my_random_number =0
-	else:
-		my_random_number = rng.randf_range(-10.0, 10.0)
-	if my_random_number >=0:
-		UpdateLight()
+	if Flickering:
+		var my_random_number
+		if NoStopingFlicking:
+			my_random_number =0
+		else:
+			my_random_number = rng.randf_range(-10.0, 10.0)
+		if my_random_number >=0:
+			UpdateLight()
 		
 func  StopFlickingLights():
 	$Timer.Stop()
+	Flickering = false
