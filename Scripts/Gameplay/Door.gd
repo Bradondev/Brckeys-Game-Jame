af1387f8cee77101f62a5ebbdd571d1c1cf33961
Mathew <mathew.tran.dev@gmail.com>
2024-02-11 20:50:03 -0800

    be able to transition between doors

---
+++ Scripts/Gameplay/Door.gd
@@ -0,0 +1,10 @@
+extends Node3D
+
+@export var RoomToGoTo = "1.tscn"
+
+func _on_area_3d_body_entered(body):
+	if body.is_in_group("Player"):
+		var currentLevel = get_tree().get_nodes_in_group("Level")
+		if currentLevel:
+			LevelLoader.SetPlayerMoveToPosition(currentLevel[0].name)
+			get_tree().change_scene_to_file("res://Scenes/Levels/" + RoomToGoTo)
