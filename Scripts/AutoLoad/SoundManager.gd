a4cfde37ade538baded98f8269cf8c619b540c54
Mathew <mathew.tran.dev@gmail.com>
2024-02-13 11:37:04 -0800

    integrate ambient sound

---
+++ Scripts/AutoLoad/SoundManager.gd
@@ -0,0 +1,26 @@
+extends Node
+
+var MusicPlayer : AudioStreamPlayer2D
+var CompleteSoundTimer : Timer
+
+
+func _ready():
+	MusicPlayer = AudioStreamPlayer2D.new()
+	add_child(MusicPlayer)
+	MusicPlayer.stream = load("res://Audio/Brandon_x4_-_Brackey_Jam_-_Ambient_Background_Music_-_Optimized.mp3")
+	MusicPlayer.play()
+	MusicPlayer.connect("finished", Callable(self, "OnMusicFinished"))
+
+	CompleteSoundTimer = Timer.new()
+	CompleteSoundTimer.wait_time = 20
+	CompleteSoundTimer.one_shot = true
+	add_child(CompleteSoundTimer)
+	CompleteSoundTimer.connect("timeout", Callable(self, "OnCompleteSoundTimerTimeout"))
+
+
+
+func OnMusicFinished():
+	CompleteSoundTimer.start()
+
+func OnCompleteSoundTimerTimeout():
+	MusicPlayer.play()
