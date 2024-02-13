cefee994db009749656df82474edefa642da2983
Brandon Martin Del Campo <brandondevprog@gmail.com>
2024-02-13 09:55:53 -0800

    Raycast popup Fix

--- Scripts/Gameplay/PopUp.gd
+++ Scripts/Gameplay/PopUp.gd
@@ -7,31 +7,32 @@ extends Node3D
 @onready var DescriptionText = $Description
 @onready var InputTextLabel = $InputLabel
 @export_color_no_alpha var OutLinecolor: Color
-
+var IsbeingLookedAt = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	visible = false
 	SetLabels()
-	SetHitBox()
 func FlipVisible():
 	if visible:
 		visible = false
 	elif !visible:
 		visible = true
+func  _physics_process(delta):
+	if IsbeingLookedAt:
+		visible = true
+	else:
+		visible =false
+		
+		
+		
+		
 
 
-func _on_hit_box_area_entered(area):
-	FlipVisible()
-	print(area)
 
-func _on_hit_box_area_exited(area):
-	FlipVisible()
-
-func SetHitBox():
-	$HitBox/CollisionShape3D.shape.size.x = PopHitBox.shape.size.x
-	$HitBox/CollisionShape3D.shape.size.z = PopHitBox.shape.size.z
-	$HitBox/CollisionShape3D.shape.size.y = PopHitBox.shape.size.y
 func SetLabels():
 	DescriptionText.text = DescriptionTextValue
 	InputTextLabel.text = InputTextLabelValue
 	DescriptionText.outline_modulate =OutLinecolor
+
+
+
