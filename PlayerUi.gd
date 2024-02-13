1547b8e32a0b8368804df814d831e46739d5f67b
Mathew <mathew.tran.dev@gmail.com>
2024-02-13 10:54:02 -0800

    work on multiple batteries showing

--- PlayerUi.gd
+++ PlayerUi.gd
@@ -2,14 +2,20 @@ extends Control
 
 
 @export var  Battery :Texture
-var BatteriesInUi 
+var BatteriesInUi
 # Called when the node enters the scene tree for the first time.
 
 
 func AddBatteryToUi():
-	var BatteryUi = TextureRect.new()
-	BatteryUi.texture =Battery
-	$batteryDisplayer.add_child(BatteryUi)
+	var batteries = LevelLoader.GetPlayer().Batteries
+	for child in $batteryDisplayer.get_children():
+		child.queue_free()
+
+	for battery in batteries:
+		var BatteryUi = TextureRect.new()
+		BatteryUi.texture =Battery
+		$batteryDisplayer.add_child(BatteryUi)
+
 func DelBatteryUi():
-	BatteriesInUi = $batteryDisplayer.get_children() 
+	BatteriesInUi = $batteryDisplayer.get_children()
 	BatteriesInUi[0].queue_free()
