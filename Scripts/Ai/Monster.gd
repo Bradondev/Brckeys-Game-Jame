e2a78a8d1a3521155d20c4a57ac2aa5a3ddb39b6
mathew-tran <mathew.tran.dev@gmail.com>
2024-02-14 13:46:48 -0800

    integrate enemy (#26)

--- Scripts/Ai/Monster.gd
+++ Scripts/Ai/Monster.gd
@@ -9,16 +9,18 @@ func _physics_process(delta):
 	rotation.x = 0
 	rotation.z = 0
 	if velocity.length() > 0:
-		# Play walk Animation 
+		# Play walk Animation
 		pass
-	
+
 	#make Monster look where its moving
-		
 
 
+func GetStateMachine():
+	return $"State Machine"
 
 func _on_kill_box_body_entered(body):
 	if body.name == "Player":
 		body.TakeDamage()
-func enableKillBox(bEnable): 
+
+func enableKillBox(bEnable):
 	$KillBOX.monitoring = bEnable
--- Scripts/Ai/MonsterIdle.gd
+++ Scripts/Ai/MonsterIdle.gd
@@ -14,8 +14,10 @@ var wander_time:float
 var target
 var Temp =0
 
+signal PassPoint
+
 func _ready():
-	
+
 	PathPoints = get_tree().get_nodes_in_group("MonsterPath")
 	RayCasts = $"../../RaycastHolder".get_children()
 func randomize_wander():
@@ -23,15 +25,18 @@ func randomize_wander():
 		Temp = 0
 	target =PathPoints[Temp].transform.origin
 	Temp+=1
-	
+	emit_signal("PassPoint")
+
+func SetTemp(index):
+	Temp = index
+	PathPoints = get_tree().get_nodes_in_group("MonsterPath")
+	target =PathPoints[Temp].transform.origin
 
 func Enter():
-	
-
 	randomize_wander()
 	print_debug("idle")
-func Update(delte:float):
 
+func Update(delte:float):
 	for Raycast in RayCasts:
 		if Raycast.is_colliding() and Raycast.get_collider() == LevelLoader.GetPlayer() :
 			print_debug(Raycast.get_collider())
@@ -42,18 +47,18 @@ func Update(delte:float):
 		target = Vector3.ZERO
 		enemy.velocity = Vector3.ZERO
 		randomize_wander()
-func Physics_Update(delta:float):	
+func Physics_Update(delta:float):
 
-	
+
 	if enemy:
 		var new_transform = enemy.transform.looking_at(target, Vector3.UP)
 		enemy.transform  = enemy.transform.interpolate_with(new_transform,rotationSpeed * delta)
 
 		enemy.velocity = -enemy.transform.basis.z * move_speed
-	
+
 	var direction = LevelLoader.GetPlayer().global_position - enemy.global_position
-	
-	
+
+
 func ChangeToRandomState():
 	var rng = RandomNumberGenerator.new()
 	var StateNumber = rng.randi_range(1, 1)
