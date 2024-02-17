e3f885b139ad5f05e5b6713b1769a60c5aaca9d3
Mathew <mathew.tran.dev@gmail.com>
2024-02-17 09:08:33 -0800

    add box around levels, make click sounds on radio

--- 3d/prefabs/radio.gd
+++ 3d/prefabs/radio.gd
@@ -4,18 +4,18 @@ extends  InterActiveObject
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	
-	
+
+
 	super._ready()
-	
+
 func InterAct():
 	if bIsOn:
 		$PopUp/Description.text = "Turn on"
 		bIsOn = false
 		$AudioStreamPlayer3D.stop()
-
+		SoundManager.PlaySFX("res://Audio/Light Switch - Off.mp3", global_position)
 	else:
 		bIsOn = true
-		
 		$AudioStreamPlayer3D.play()
 		$PopUp/Description.text = "Turn off"
+		SoundManager.PlaySFX("res://Audio/Light Switch - On.mp3", global_position)
